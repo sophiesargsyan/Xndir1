c6db3e4d595d089fb0614c63dca2879fcb6a2c9d
Sophie Sargsyan <144236558+sophiesargsyan@users.noreply.github.com>
2024-05-11 17:13:14 +0400

    created 3 h2 elements...

--- main.js
+++ main.js
@@ -58,3 +58,28 @@ document.body.appendChild(secondForm);
 // Որոնման միջոցով գտնել այդ էլեմենտը
 let foundElem2 = document.querySelector('[name="login"]');
 
+// 5)Ստեղծել երեք երկրորդ կարգի վերնագրեր, որոնցից մեկի կոնտենտն է "JavaScript DOM lessons"
+let header1 = document.createElement('h2');
+header1.textContent = "Header 1";
+
+let header2 = document.createElement('h2');
+header2.textContent = "JavaScript DOM lessons";
+
+let header3 = document.createElement('h2');
+header3.textContent = "Header 3";
+
+document.body.appendChild(header1);
+document.body.appendChild(header2);
+document.body.appendChild(header3);
+
+// Որոնման միջոցով գտնել այդ էլեմենտը և դուրս բերել կոնսոլում
+let headers = document.getElementsByTagName('h2');
+let foundHeader;
+for (let i = 0; i < headers.length; i++) {
+  if (headers[i].textContent === "JavaScript DOM lessons") {
+    foundHeader = headers[i];
+    break;
+  }
+}
+console.log(foundHeader);
+
